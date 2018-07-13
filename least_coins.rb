adbacb3880c9b385640c0ce7e4708301e52da420
mggbhn <maggiebswim@gmail.com>
2018-07-13 13:39:45 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -2,6 +2,17 @@
 
 def least_coins(cents)
 
-#Code your answer here!
+least_coins = {} 
+
+numquarters = cents / 25 
+numdimes = (cents - (numquarters*25)) / 10
+numnickels = (cents - (numquarters*25) - (numdimes*10))/ 5
+numpennies = (cents - (numquarters*25) - (numdimes*10) - (numnickels*5))/1
+
+least_coins = { :quarters => numquarters , :dimes => numdimes , :nickels => numnickels , :pennies => numpennies }
 
 end
+
+
+
+puts least_coins(29)
